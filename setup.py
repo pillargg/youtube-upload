c6f06eecf8f0de29085b6f6e07a225b3a2cdd277
chand1012 <chandler@chand1012.dev>
2021-01-09 21:28:05 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setuptools.setup(
     name='pillar-youtube-upload',
-    version='0.1.2',
+    version='0.2.0',
     author="PillarGG",
     author_email='chandler@pillar.gg',
     description='Upload YouTube videos from Python and more.',
